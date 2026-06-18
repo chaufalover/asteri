5d32a3719954053e96be213d34674a1e6e8f9ff9
tom <tacuresancheztomas@gmail.com>
2026-06-18 18:00:17 -0500

    Creacion del proyecto

---
+++ src/main/java/com/conex/asteri/AsteriApplication.java
@@ -0,0 +1,13 @@
+package com.conex.asteri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsteriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsteriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conex/asteri/AsteriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conex.asteri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsteriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
